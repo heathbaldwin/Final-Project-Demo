5a094ba4c8ca08eaadba5a7b7c91dd4a53609950
heathbaldwin <105754991+heathbaldwin@users.noreply.github.com>
2023-04-10 10:09:17 -0400

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,40 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to DecaCalc!</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best decathalon score calculator on the internet.
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Calculate Score &rarr;</h3>
+            <p>Enter marks to see what your decathalon score would be.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Rankings&rarr;</h3>
+            <p>View the world rankings for the decathalon.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Articles &rarr;</h3>
+            <p>
+              Current news articles regarding decathaletes and their paths to
+              success.
+            </p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>History &rarr;</h3>
+            <p>View top marks across world history.</p>
           </a>
         </div>
       </main>
